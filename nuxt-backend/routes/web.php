3609e7a5780b0d7144feccd5842998fcd7882482
AKASH <mdrakash32@gmail.com>
2023-11-23 01:09:41 +0600

    make question using open api

--- nuxt-backend/routes/web.php
+++ nuxt-backend/routes/web.php
@@ -1,39 +1,49 @@
 <?php
 
+use App\Models\Question;
 use Illuminate\Support\Facades\Route;
 use OpenAI\Laravel\Facades\OpenAI;
 use Illuminate\Support\Facades\Http;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    $search = "make a json array for list to questions categories for interview questtion";
-    $data = Http::withHeaders([
-        'Content-Type' => 'application/json',
-        'Authorization' => 'Bearer '.env('OPENAI_API_KEY'),
-    ])
-    ->post('https://api.openai.com/v1/chat/completions',[
-         'model' => 'gpt-3.5-turbo',
-            'messages' => [
-               [ 
-                    "role" => 'user',
-                    'content' => $search
-                ]
-            ],
-            'temperature' => 0.5,
-            'max_tokens' => 200,
-            'top_p' => 1.0,
-            'frequency_penalty' => 0.52,
-            'presence_penalty' => 0.5,
-            'stop' =>["11."],
-        ]);
-    print_r($data['choices'][0]['message']);
+    $data = json_decode(MakeQuestion());
+    dump($data);
+    foreach ($data->questions as  $question) {
+        Question::create(['question'=>$question]);
+        dump($question);
+    }
+    
+    return 'success';
 });
+
+function MakeQuestion()
+{
+    try {
+        $data = Http::timeout(60)->withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer '.env('OPENAI_API_KEY'),
+        ])
+        ->post('https://api.openai.com/v1/chat/completions',[
+                'model' => 'gpt-3.5-turbo-1106',
+                'messages' => [
+                    [ 
+                        "role" => 'system',
+                        'content' => 'Generate JSON response'
+                    ],
+                    [
+                        'role' => 'user',
+                        'content' => 'make 50 unique questions for interview warm up it support category in json format no numbering'
+                    ],
+                    [
+                        'role' => 'user',
+                        'content' => 'make array key name as questions'
+                    ]
+                    
+                ],
+                'response_format'=> [ "type"=> "json_object" ],
+            ]);
+        return $data['choices'][0]['message']['content'];
+    } catch (\Throwable $th) {
+        throw $th;
+    }
+}
